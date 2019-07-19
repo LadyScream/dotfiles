f482101ad8db11caccb1ef7278181b4a72668e87
ladyscream <the.lady.scream@gmail.com>
2019-07-19 05:50:34 -0500

    Changed autostart apps

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -2,10 +2,11 @@
 ~/.fehbg &
 pkill -9 cbatticon
 pkill -9 pa-applet
+pkill -9 udiskie
 pkill -9 megasync
 xset +fp /home/ladyscream/.local/share/fonts
 xset fp rehash
-xinput set-prop 'SynPS/2 Synaptics TouchPad' 292 0 &
+xinput set-prop 'SynPS/2 Synaptics TouchPad' 305 0 &
 slstatus &
 megasync &
 pa-applet &
@@ -13,9 +14,7 @@ nm-applet &
 cbatticon &
 setxkbmap -option compose:ralt &
 unclutter --timeout 3 &
-compton &
-transmission-daemon &
-~/.flexget/bin/flexget daemon start -d &
-thunderbird &
-play-with-mpv &
+compton -b
+udiskie &
+dunst &
 betterlockscreen -u ~/.lockscreen.png &
